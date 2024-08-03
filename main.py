de6fbb7daa8d7b0fda912d0c9239672dea6b7278
Elena Lomakina <lomakina0612@gmail.com>
2024-08-04 00:17:08 +0200

    arithmetic operations

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("yes")
+a = 2 * 3
+print("a = 2 * 3 = ", a)
+print("type(a) - ", type(a))
+
+b = (3*3 + 8) / 3
+print("b = (3*3 + 8) / 3 = ", b)
+print("type(b) - ", type(b))
+
+c = 8 // 3
+print("c = 8 // 3 = ", c)
+print("type(c) - ", type(c))
+
+d = 8 % 3
+print("d = 8 % 3 = ", d)
+print("type(d) - ", type(d))
+
+e = 5 ** 2
+print("e = 5 ** 2 = ", e)
+print("type(e) - ", type(e))
+
+f = 'Hello ' + 'world'
+print("f = 'Hello ' + 'world' = ", f)
+print("type(f) - ", type(f))
+
+
+str1 = 'first text variable'
+str2 = "second text variable"
+print("str1 = ", str1)
+print("str2 = ", str2)
