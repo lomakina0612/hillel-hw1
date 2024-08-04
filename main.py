c0d2fb9f81c42cf7110f863b6196f31381070f74
Elena Lomakina <lomakina0612@gmail.com>
2024-08-04 23:27:55 +0200

    work with slices

--- main.py
+++ main.py
@@ -27,3 +27,12 @@ str1 = 'first text variable'
 str2 = "second text variable"
 print("str1 = ", str1)
 print("str2 = ", str2)
+
+
+initial_string = "one_random_string"
+print("first 10 characters: ", initial_string[:10])
+print("10 characters from 3rd: ", initial_string[2:12])
+print("last 10 characters: ", initial_string[-10:])
+print("reversed_string: ", initial_string[::-1])
+print("even-indexed: ", initial_string[::2])
+print("odd-indexed: ", initial_string[1::2])
